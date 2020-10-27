d98f36c92f34ce89d9ea0a7c8c6d278d7b76eeda
Nick Sotula <so2niko@gmail.com>
2020-10-27 20:35:28 +0200

    add head writing vs get request

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/html; charset=utf-8",
+      });
     res.send('Hello, I am a BOT');
+    
 });
 
 app.listen(80);
