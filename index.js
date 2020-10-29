262068ee7c9582a590a474a3637c14dfcda1f725
Nick Sotula <so2niko@gmail.com>
2020-10-29 12:58:56 +0200

    fix: add PORT and script start

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
-
 const app = express();
 
+const PORT = process.env.PORT || 80;
+
 app.get('/', (req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/html; charset=utf-8",
-      });
-    res.send('Hello, I am a BOT');
-    
+    // res.writeHead(200, {
+    //     "Content-Type": "text/html; charset=utf-8",
+    //   });
+    res.end('<h2>Hello, I am a BOT</h2>'); 
 });
 
-app.listen(5000);
+app.get('/about', (req, res) => {
+  // res.writeHead(200, {
+  //     "Content-Type": "text/html; charset=utf-8",
+  //   });
+  res.end('<h2>About bot</h2>'); 
+});
+
+app.listen(PORT, 
+    () => console.log('Server has been started'));
